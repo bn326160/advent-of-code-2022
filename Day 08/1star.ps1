bb79aec6fa6b1fd6c2bf42b2174045ea6190d47b
Bram Beirens <Bram.Beirens@belfius.be>
2022-12-16 16:28:53 +0100

    Day 8

--- Day 08/1star.ps1
+++ Day 08/1star.ps1
@@ -1,4 +1,82 @@
 #!/usr/bin/env pwsh
 
-$content = Get-Content -Path .\sample.txt -Raw
-$array = @($content -split [System.Environment]::NewLine)
+$DebugPreference = 'Continue'
+
+$content = Get-Content -Path .\input.txt -Raw
+$lines = @($content -split [System.Environment]::NewLine)
+
+for ($i = 0; $i -lt $lines.Count; $i++) {
+    $lines[$i] = $lines[$i].ToCharArray() | % {iex $_} 
+}
+
+function Get-Visibility($row, $col) {
+
+    $val = $lines[$row][$col]
+    Write-Debug "Tree on column $($col+1), row $($row+1) with height $val"
+
+    $i = $row
+    do {
+        if ($i -eq 0) {
+            Write-Host "Tree on column $($col+1), row $($row+1) with height $val is visible from the top"
+            return $true
+        }
+        $i--
+        if ($lines[$i][$col] -ge $val) {
+            break
+        }
+    } while ($i -ge 0)
+
+    $i = $col
+    do {
+        if ($i -eq 0) {
+            Write-Host "Tree on column $($col+1), row $($row+1) with height $val is visible from the left"
+            return $true
+        }
+        $i--
+        if ($lines[$row][$i] -ge $val) {
+            break
+        }
+    } while ($i -ge 0)
+
+    $i = $row
+    do {
+        if ($i -eq $lines.Count-1) {
+            Write-Host "Tree on column $($col+1), row $($row+1) with height $val is visible from the bottom"
+            return $true
+        }
+        $i++
+        if ($lines[$i][$col] -ge $val) {
+            break
+        }
+    } while ($i -le $lines.Count-1)
+
+    $i = $col
+    do {
+        if ($i -eq $lines[$row].Count-1) {
+            Write-Host "Tree on column $($col+1), row $($row+1) with height $val is visible from the right"
+            return $true
+        }
+        $i++
+        if ($lines[$row][$i] -ge $val) {
+            break
+        }
+    } while ($i -le $lines[$row].Count-1)#>
+    
+    return $false
+}
+
+$visibleTrees = 0
+$rowI = 0
+foreach ($row in $lines) {
+    $colI = 0
+    foreach ($col in $row) {
+        $visible = Get-Visibility -Row $rowI -Col $colI
+        if ($visible) {
+            $visibleTrees++
+        }
+        $colI++
+    }
+    $rowI++
+}
+
+Write-Output $visibleTrees
--- Day 08/2stars.ps1
+++ Day 08/2stars.ps1
@@ -1,1 +1,83 @@
 #!/usr/bin/env pwsh
+
+#$DebugPreference = 'Continue'
+
+function Reduce($initial, $sb)
+{
+  begin { $result = $initial }
+  process { $result = & $sb $result $_ }
+  end { $result }
+}
+
+$content = Get-Content -Path .\input.txt -Raw
+$lines = @($content -split [System.Environment]::NewLine)
+
+for ($i = 0; $i -lt $lines.Count; $i++) {
+    $lines[$i] = $lines[$i].ToCharArray() | % {iex $_} 
+}
+
+function Get-ScenicScore($row, $col) {
+
+    Write-Debug "Tree on column $($col+1), row $($row+1) with height $($lines[$row][$col])"
+
+    $scoreArr = [int[]]::new(4)
+
+    $score = 0
+    for ($i = $row; $i -gt 0; $i--) {
+        $score++
+        if ($i -ne $row -And $lines[$i][$col] -ge $lines[$row][$col]) {
+            $score--
+            $i = 0
+        }
+    }
+    $scoreArr[0] = $score
+
+    $score = 0
+    for ($i = $row; $i -lt $lines.Count; $i++) {
+        $score++
+        if ($i -ne $row -And $lines[$i][$col] -ge $lines[$row][$col]) {
+            $score--
+            $i = $lines.Count
+        }
+    }
+    $scoreArr[1] = $score
+
+    $score = 0
+    for ($i = $col; $i -gt 0; $i--) {
+        $score++
+        if ($i -ne $col -And $lines[$row][$i] -ge $lines[$row][$col]) {
+            $score--
+            $i = 0
+        }
+    }
+    $scoreArr[2] = $score
+
+    $score = -1 # Why?
+    for ($i = $col; $i -lt $lines[$row].Count; $i++) {
+        $score++
+        if ($i -ne $col -And $lines[$row][$i] -ge $lines[$row][$col]) {
+            $score--
+            $i = $lines[$row].Count
+        }
+    }
+    $scoreArr[3] = $score
+
+    Write-Debug "$scoreArr"
+
+    return $scoreArr | Reduce 1 { param($x, $y) $x * $y }
+}
+
+
+$topScore = 0
+$rowI = 0
+foreach ($row in $lines) {
+    $colI = 0
+    foreach ($col in $row) {
+        $score = Get-ScenicScore -Row $rowI -Col $colI
+        $topScore = if ($score -gt $topScore) { $score } else { $topScore }
+        $colI++
+    }
+    $rowI++
+}
+
+Write-Output $topScore
